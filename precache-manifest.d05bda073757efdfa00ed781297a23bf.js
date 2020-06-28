90a535cf4ceda34b885ad733f55ba607c5e918ad
Takato Cox <takatohockey@gmail.com>
2020-06-27 22:48:08 -0700

    Updates

---
+++ precache-manifest.d05bda073757efdfa00ed781297a23bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "038e5e084db5ce82c8fec5707bfc60ad",
+    "url": "/ReactRecipes/index.html"
+  },
+  {
+    "revision": "9080e1b3c32866ec9995",
+    "url": "/ReactRecipes/static/css/main.9572605f.chunk.css"
+  },
+  {
+    "revision": "facc40d13830b340c637",
+    "url": "/ReactRecipes/static/js/2.19fb2ed3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactRecipes/static/js/2.19fb2ed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9080e1b3c32866ec9995",
+    "url": "/ReactRecipes/static/js/main.d17c31f0.chunk.js"
+  },
+  {
+    "revision": "1fe7620a6d43607a76e1",
+    "url": "/ReactRecipes/static/js/runtime-main.ab603761.js"
+  }
+]);
